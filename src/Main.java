1fc66db2b5347d9e3610606ec13ff4d296bbf990
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2022-09-13 15:52:21 -0400

    juicer

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,28 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Arrays;
-
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList; // Import because I'm a C# enjoyer
+import java.util.Scanner;   // Import to read input
+import java.util.Arrays;    // Import to format console output
 
 
 public class Main {
-    public static void main(String[] args) {
-        int totalEntries = 0;
-        ArrayList<Double> numbers = new ArrayList<>(130);
-        try {
-            File myObj = new File("/Users/matteo/dev/github/adamNums/src/data.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                numbers.add(Double.valueOf(data));
 
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+    static ArrayList<ArrayList<Double>> output = new ArrayList<>();
 
-//        for (double number: numbers) {
-//            System.out.println(number);
-//        }
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<Double> numbers = read_input_file();
 
         double target = 1646.13;
         double variance = 0.5;
+
+        // Time Logging
         long start = System.currentTimeMillis();
-
         Thread newThread = new Thread(() -> {
-            while (true){
-
+            while (true) {
                 long finish = System.currentTimeMillis();
                 long timeElapsed = finish - start;
-                System.out.println(timeElapsed);
+                System.out.println(timeElapsed / 1000 + " seconds elapsed");
                 try {
                     Thread.sleep(10000);
                 } catch (InterruptedException e) {
@@ -48,37 +33,72 @@ public class Main {
         });
         newThread.start();
 
-        sum_up(new ArrayList<Double> (numbers), target);
-    }
 
+        // Start recursive task
+        sum_up(new ArrayList<Double>(numbers), target);
+
+        write_output_to_file();
+    }
 
 
     static void sum_up_recursive(ArrayList<Double> numbers, double target, ArrayList<Double> partial) {
         double s = 0;
-        for (double x: partial) s += x;
+        for (double x : partial) s += x;
 
         //if (target - s < 0.5 && target - s > - 0.5)
-        if (s == target)
-            System.out.println("sum("+Arrays.toString(partial.toArray())+")="+target);
-        if (s >= target)
+        if (s == target) {
+            System.out.println(Arrays.toString(partial.toArray()));
+            output.add(partial);
+        }
+
+        if (s >= target) {
             return;
-        for(int i=0;i<numbers.size();i++) {
+        }
+
+        for (int i = 0; i < numbers.size(); i++) {
             ArrayList<Double> remaining = new ArrayList<Double>();
             double n = numbers.get(i);
-            for (int j=i+1; j<numbers.size();j++) remaining.add(numbers.get(j));
+
+            for (int j = i + 1; j < numbers.size(); j++) {
+                remaining.add(numbers.get(j));
+            }
             ArrayList<Double> partial_rec = new ArrayList<Double>(partial);
             partial_rec.add(n);
-            sum_up_recursive(remaining,target,partial_rec);
+            sum_up_recursive(remaining, target, partial_rec);
         }
     }
+
     static void sum_up(ArrayList<Double> numbers, double target) {
-        sum_up_recursive(numbers,target,new ArrayList<Double>());
+        sum_up_recursive(numbers, target, new ArrayList<Double>());
     }
 
+    public static ArrayList<Double> read_input_file() {
+        int totalEntries = 0;
+        ArrayList<Double> numbers = new ArrayList<>(130);
+        try {
+            File myObj = new File("src/data.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                numbers.add(Double.valueOf(data));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return numbers;
+    }
 
+    public static void write_output_to_file() throws IOException {
+        PrintWriter writer = new PrintWriter("src/output.txt", StandardCharsets.UTF_8);
 
+        for (ArrayList<Double> list : output) {
+            writer.println(Arrays.toString(list.toArray()));
+        }
 
-
+        writer.close();
+    }
 
 
 //        ArrayList<Double> duplicates = new ArrayList<>(130);
